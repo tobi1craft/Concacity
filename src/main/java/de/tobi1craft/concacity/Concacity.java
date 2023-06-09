239ec102c51e767f69b35084797cb36aef92c7d3
Tobias Hauck <tobi1craft.mc@gmail.com>
2023-06-09 14:17:44 +0200

    first commit

---
+++ src/main/java/de/tobi1craft/concacity/Concacity.java
@@ -0,0 +1,10 @@
+package de.tobi1craft.concacity;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Concacity implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/tobi1craft/concacity/client/ConcacityClient.java
@@ -0,0 +1,13 @@
+package de.tobi1craft.concacity.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ConcacityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
