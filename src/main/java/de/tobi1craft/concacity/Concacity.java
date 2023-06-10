cdeea3487825a5a52eb24ad69d1cf994e8acfd67
Tobias Hauck <tobi1craft.mc@gmail.com>
2023-06-10 07:38:30 +0200

    first commit

--- src/main/java/de/tobi1craft/concacity/Concacity.java
+++ src/main/java/de/tobi1craft/concacity/Concacity.java
@@ -1,10 +1,12 @@
 package de.tobi1craft.concacity;
 
+import de.tobi1craft.concacity.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class Concacity implements ModInitializer {
+    public static String ID = "concacity";
     @Override
     public void onInitialize() {
-
+        ModItems.registerModItems();
     }
 }
--- src/main/java/de/tobi1craft/concacity/item/ModItems.java
+++ src/main/java/de/tobi1craft/concacity/item/ModItems.java
@@ -1,5 +1,21 @@
 package de.tobi1craft.concacity.item;
 
+import de.tobi1craft.concacity.Concacity;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
 
+    public static final Item test_item = registerItem("test_item", new Item(new FabricItemSettings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Concacity.ID, name), item);
+    }
+
+    public static void registerModItems() {
+
+    }
 }
